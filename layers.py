Fix Linear Layer Initialization, Gradient Updates and Stale Gradient References in Network

layers.py
@@ -23,14 +23,15 @@ class Layer:
 
 class Linear(Layer):
     def __init__(self, inF, outF, bias=True, dtype=np.float32):
+        super().__init__()
         self.layers_name = self.__class__.__name__
         self.trainable = True
         lim = 1 / np.sqrt(inF) # Only inF used to calculate the limit, avoid saturation..
         self.w = np.random.uniform(-lim, lim, (outF, inF)).astype(dtype) # torch style (outF, inF)
         self.b = np.random.randn(outF).astype(dtype) * 0.1 if bias else None
-        self.params = (self.w, self.b)
+        self.params = [self.w, self.b] if bias else [self.w]
+        self.grads = [np.zeros_like(p) for p in self.params]
         self.inShape, self.outShape = (inF,), (outF,)
-        super().__init__()
 
     def forward(self, z):
         self.z = z
@@ -38,6 +39,9 @@ class Linear(Layer):
 
     def backward(self, TopGrad):
         self.zGrad, self.wGrad, self.bGrad = affin_trans_backward(TopGrad, self.z, self.w)
+        self.grads[0] = self.wGrad
+        if self.b is not None:
+            self.grads[1] = self.bGrad
         return self.zGrad
 
 class BatchNorm1D(Layer):

network.py
@@ -60,12 +60,14 @@ class Network:
         """
         if not self.optimizer_built:
             self.params.extend(itertools.chain(*[layer.params for layer in self.layers]))
-            self.grads.extend(itertools.chain(*[layer.grads for layer in self.layers]))
             self.first_moments = [np.zeros_like(param) for param in self.params]
             self.second_moments = [np.zeros_like(param) for param in self.params]
             self.time_step = 1
             self.optimizer_built = True
-        for param, grad, first_moment, second_moment in zip(self.params, self.grads,
+        
+        current_grads = list(itertools.chain(*[layer.grads for layer in self.layers]))
+        
+        for param, grad, first_moment, second_moment in zip(self.params, current_grads,
                                                             self.first_moments, self.second_moments):
             first_moment *= beta_1
             first_moment += (1 - beta_1) * grad